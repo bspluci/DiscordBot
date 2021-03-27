046fbf22949c5d06894eb4dd3ec8fae7838b2243
bspluci <47733415+bspluci@users.noreply.github.com>
2021-03-27 21:01:59 +0900

    aa

--- commands/utility/event.js
+++ commands/utility/event.js
@@ -191,17 +191,20 @@ module.exports = {
 
          async function findTeam() {
             for (let i = 0; i < thisTeam.length; i++) {
-               await moveChannel(thisTeam[i], i);
+               await moveChannel(thisTeam[i], childChannel[i]);
             }
          }
 
-         function moveChannel(team, i) {
+         let num = 0;
+
+         function moveChannel(team, child) {
             return new Promise((resolve) => {
+               num++;
                setTimeout(() => {
                   for (let s = 0; s < team.length; s++) {
                      const setChannelUser = MGCC.get(baseChannel).members.map((user) => {
                         if (user.user.username === team[s].user.username) {
-                           MGMC.get(user.user.id).voice.setChannel(MGCC.get(child[i]));
+                           MGMC.get(user.user.id).voice.setChannel(MGCC.get(child[num]));
                         }
                      });
                      resolve(setChannelUser);
